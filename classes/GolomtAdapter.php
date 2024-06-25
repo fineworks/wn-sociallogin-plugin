a0d1b3700b495ce2c754c4ef5bd9ae48b4ed58cb
Nominerdene <nmnrdn@yahoo.com>
2024-06-25 16:54:17 +0800

    Golomt check log

--- classes/GolomtAdapter.php
+++ classes/GolomtAdapter.php
@@ -44,7 +44,11 @@ class GolomtAdapter
 
                 $this->signature = $this->encodeSignature(hash("sha256", $jsonParams));
 
-                Log::info('GolomtAdapter:authenticate: '.$this->signature);
+                Log::info('GolomtAdapter:Token: '.$token);
+                Log::info('GolomtAdapter:URL: '.self::CHECK_URL);
+                Log::info('GolomtAdapter:Method: POST');
+                Log::info('GolomtAdapter:Header: '.json_encode($this->postHeaders()));
+                Log::info('GolomtAdapter:Body: '.$jsonParams);
                 
                 $response = $this->post(self::CHECK_URL, $requestData, $this->postHeaders(), true);
                 Log::info('GolomtAdapter:response: '.print_r($response, true));
